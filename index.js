f6c741d7f751950486f8f53d28f3e8de664e6410
Yerko Palma <yerko.palma@usach.cl>
2016-12-01 17:27:38 +0000

    [skip ci] updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const senadores = require('senadores-base')
 const pMap = require('p-map')
 const pAll = require('p-all')
+const utils = require('./utils')
+const getViajesNacionales = utils.getViajesNacionales
+const getPeriodo = utils.getPeriodo
+const getViajesExtranjeros = utils.getViajesExtranjeros
 
 // Get travels details for senators
 // (any, obj) -> arr
@@ -21,19 +25,17 @@ module.exports = function senadoresViajes (query, options) {
     switch (options.tipo) {
       case 'todos':
         const actions = [
-          // () => getViajesNacionales(senador, getPeriodo(options.periodo)),
-          // () => getViajesExtranjeros(senador, getPeriodo(options.periodo))
+          () => getViajesNacionales(senador, getPeriodo(options.periodo)),
+          () => getViajesExtranjeros(senador, getPeriodo(options.periodo))
         ]
         return pAll(actions).then(result => {
           if (options.incluyeSenador) return { senador, nacionales: result[0], extranjeros: result[1] }
           return { nacionales: result[0], extranjeros: result[1] }
         })
       case 'nacionales':
-        // return getViajesNacionales(senador, getPeriodo(options.periodo), options.incluyeSenador)
-        break
+        return getViajesNacionales(senador, getPeriodo(options.periodo), options.incluyeSenador)
       case 'extranjeros':
-        // return getViajesExtranjeros(senador, getPeriodo(options.periodo), options.incluyeSenador)
-        break
+        return getViajesExtranjeros(senador, getPeriodo(options.periodo), options.incluyeSenador)
       default:
         throw new Error(`[senadores-viajes]: Error - tipo de retorno '${options.tipo}' no conocido.`)
     }
